5192afddc717dd57e399bf18bb53beebb7e1415d
salman <salman.pirani@gmail.com>
2019-09-01 14:06:21 +0500

    class3

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,11 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{
+  path:'login',
+  component:LoginComponent
+},
+{
+path:'home',
+component:HomeComponent
+}
+,
+{
+  path:'',
+  component:LoginComponent
+},
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+import {Component} from '@angular/core';
+import { HomeComponent } from './home/home.component';
+
+ 
+
+
+
+
+
+ 
