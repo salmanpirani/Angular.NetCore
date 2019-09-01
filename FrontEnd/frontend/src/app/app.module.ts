541f5812d12df20c0886da20dda6e6a798d3a279
salman <salman.pirani@gmail.com>
2019-09-01 13:09:15 +0500

    angular setup

--- FrontEnd/frontend/src/app/app.module.ts
+++ FrontEnd/frontend/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LogComponent } from './log/log.component';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogComponent,
     LoginComponent
   ],
   imports: [
